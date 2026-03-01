df05d723835a2c70e916545136f63b567ef17975
Jeff Pine <iservice.pinheiro@gmail.com>
2026-03-01 20:57:15 -0300

    cors config criado para reparar conexão com o front

--- src/main/java/com/pinheiro/estoque_fabrica/controller/ProducaoController.java
+++ src/main/java/com/pinheiro/estoque_fabrica/controller/ProducaoController.java
@@ -1,6 +1,7 @@
 package com.pinheiro.estoque_fabrica.controller;
 import com.pinheiro.estoque_fabrica.dto.PlanoProducaoDTO;
 import com.pinheiro.estoque_fabrica.service.OtimizacaoProducaoService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
